bf3572f216f496c1640dceb86c33dc59c0cd574b
buhe <bugu1986@126.com>
2021-11-03 09:38:47 +0800

    init lalrpop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+#[macro_use]
+extern crate lalrpop_util;
+lalrpop_mod!(pub setp1); // syntesized by LALRPOP
+
+#[test]
+fn setp1() {
+    assert!(setp1::TermParser::new().parse("22").is_ok());
+    assert!(setp1::TermParser::new().parse("(22)").is_ok());
+    assert!(setp1::TermParser::new().parse("((((22))))").is_ok());
+    assert!(setp1::TermParser::new().parse("((22)").is_err());
+}
+
 fn main() {
     println!("Hello, world!");
 }
