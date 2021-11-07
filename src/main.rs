f61f8a3a6c688993e6f9f4783be8266f57c2c8d1
buhe <bugu1986@126.com>
2021-11-07 19:53:52 +0800

    add debug config

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,4 @@
-#[macro_use]
-extern crate lalrpop_util;
-lalrpop_mod!(pub setp1); // syntesized by LALRPOP
-
-#[test]
-fn setp1() {
-    assert!(setp1::TermParser::new().parse("22").is_ok());
-    assert!(setp1::TermParser::new().parse("(22)").is_ok());
-    assert!(setp1::TermParser::new().parse("((((22))))").is_ok());
-    assert!(setp1::TermParser::new().parse("((22)").is_err());
-}
 
 fn main() {
-    println!("{}", setp1::TermParser::new().parse("22").unwrap());
+    println!("hello");
 }
