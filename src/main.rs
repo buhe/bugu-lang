1aa224d49b96837b3158769058c725309114dfea
buhe <bugu1986@126.com>
2021-12-28 21:37:47 +0800

    add example

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
+use std::fs;
 
-fn main() {
-    println!("hello");
+use lib_rv32_asm::assemble_program;
+
+
+fn main() -> std::io::Result<()> {
+    let i = std::env::args().nth(1).expect("no input");
+    let src = fs::read_to_string(i).expect("src not existed.");
+    println!("With text:\n{}", src);
+    // let bin = assemble_program("").unwrap();
+    Ok(())
 }
